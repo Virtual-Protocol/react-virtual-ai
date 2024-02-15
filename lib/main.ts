34328e2e6006a75639afbf6e4b8b414cc855f1d7
Chin Jia Xiong <jiaxiong@Chins-MacBook-Pro.local>
2024-02-16 00:47:23 +0800

    Add more exports

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,6 @@
 export { useVirtualAI } from "./hooks/useVirtualAI";
 export { Button } from "./components/Button";
+export { AICharacter } from "./components/AICharacter/AICharacter";
+export { CharacterRoom } from "./components/CharacterRoom/CharacterRoom";
+export { CharacterScene } from "./components/CharacterScene/CharacterScene";
+export { Input } from "./components/Input/Input";
