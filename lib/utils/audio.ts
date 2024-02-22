5fb634b6f1108ce2028a9b8c5054fa627f86561c
Chin Jia Xiong <jiaxiong@Chins-MacBook-Pro.local>
2024-02-22 16:23:18 +0700

    Remove unused function

--- lib/utils/audio.ts
+++ lib/utils/audio.ts
@@ -2,52 +2,6 @@ import { currentVrm } from "../components/AICharacter/AICharacter";
 // @ts-ignore
 import { LipSync } from "./lipsync";
 
-//   const inputName = "input.webm";
-//   const outputName = "output.mp3";
-
-//   const fileData = new Uint8Array(await webmBlob.arrayBuffer());
-
-//   await ffmpeg.writeFile(inputName, fileData);
-//   await ffmpeg.exec(["-i", inputName, outputName]);
-
-//   const outputData = await ffmpeg.readFile(outputName);
-//   const outputBlob = new Blob([outputData], { type: "audio/mp3" });
-
-//   console.log("here");
-
-//   return outputBlob;
-// };
-
-export const convertToDownloadFileExtension = async (
-  webmBlob: Blob,
-  downloadFileExtension: string
-): Promise<Blob> => {
-  const FFmpeg = await import("@ffmpeg/ffmpeg");
-  const ffmpeg = FFmpeg.createFFmpeg({
-    log: false,
-    corePath: "https://unpkg.com/@ffmpeg/core@0.11.0/dist/ffmpeg-core.js",
-  });
-  await ffmpeg.load();
-
-  const inputName = "input.webm";
-  const outputName = `output.${downloadFileExtension}`;
-
-  ffmpeg.FS(
-    "writeFile",
-    inputName,
-    new Uint8Array(await webmBlob.arrayBuffer())
-  );
-
-  await ffmpeg.run("-i", inputName, outputName);
-
-  const outputData = ffmpeg.FS("readFile", outputName);
-  const outputBlob = new Blob([outputData.buffer], {
-    type: `audio/${downloadFileExtension}`,
-  });
-
-  return outputBlob;
-};
-
 let lipsyncService: any | undefined;
 
 export const startLipSync = async (
