f2b3744e7d84541cb8d2d48f2b97a48f74fc1dbc
Chin Jia Xiong <jiaxiong@Chins-MacBook-Pro.local>
2024-03-15 11:39:58 +0800

    Remove comment

--- lib/main.ts
+++ lib/main.ts
@@ -1,5 +1,3 @@
-// import "./index.css";
-
 export { useVirtualAI } from "./hooks/useVirtualAI";
 export { AICharacter } from "./components/AICharacter/AICharacter";
 export { CharacterRoom } from "./components/CharacterRoom/CharacterRoom";
