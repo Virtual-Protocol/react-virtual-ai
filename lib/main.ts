4631f550e32132efbbe47205e9d2bb05ca7076c4
Chin Jia Xiong <jiaxiong@Chins-MacBook-Pro.local>
2024-03-15 12:43:39 +0800

    Export helper functions

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,7 @@
 export { useVirtualAI } from "./hooks/useVirtualAI";
+export { UNSAFE_initAccessToken } from "./utils/initAccessToken";
+export { validateJwt } from "./utils/jwt";
+export { getQuotedTexts } from "./utils/string";
 export { AICharacter } from "./components/AICharacter/AICharacter";
 export { CharacterRoom } from "./components/CharacterRoom/CharacterRoom";
 export { CharacterScene } from "./components/CharacterScene/CharacterScene";
